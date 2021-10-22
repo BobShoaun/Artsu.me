71881d37350340e67022a8681ab870a04518d8a5
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-10-22 17:36:03 -0400

    portfolio page

--- frontend/src/PortfolioPage/index.jsx
+++ frontend/src/PortfolioPage/index.jsx
@@ -1,18 +1,29 @@
+import { Link } from "react-router-dom";
+
 const PortfolioPage = () => {
   return (
-    <main className="dark:bg-gray-800 px-2 py-20">
-      <section className="container mx-auto">
-        <h1 className="dark:text-white text-3xl">Name Surname</h1>
+    <main className="dark:bg-gray-900 px-2">
+      <section className="container mx-auto pb-20">
+        <header className="flex items-center gap-10 py-10">
+          <h1 className="dark:text-white text-3xl">Name Surname</h1>
+          <Link className="ml-auto text-gray-200">Artworks</Link>
+          <Link className="text-gray-200">Contact Me</Link>
+          <Link className="text-gray-200">Follow</Link>
+        </header>
 
         <div className="flex">
-          <h1 className="dark:text-white text-4xl">Welcome to my portfolio</h1>
-          <p className="dark:text-white">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s Lorem Ipsum is simply dummy text of the
-            printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s
-          </p>
+          <div>
+            <h1 className="dark:text-white text-4xl mb-4">
+              Welcome to my portfolio!
+            </h1>
+            <p className="dark:text-gray-300">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s
+            </p>
+          </div>
         </div>
       </section>
     </main>

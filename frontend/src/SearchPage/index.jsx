695e3badae54a6914b5fb6cea91f72e3ed6d5521
SR <1219361656@qq.com>
2021-11-06 14:34:43 -0400

    Update index.jsx
    Change display Style

--- frontend/src/SearchPage/index.jsx
+++ frontend/src/SearchPage/index.jsx
@@ -5,7 +5,8 @@ import { artworks } from "../artworks.json";
 import { tags } from "../tags.json"
 
 const SearchPage = () => {
-  console.log(artworks.length)
+  var href = location.href;
+  console.log(href.match(/([^\/]*)\/*$/)[1]);
 
   const primary = { main: "rose-600", light: "rose-500", dark: "rose-700" };
   const secondary = { main: "teal-700", light: "teal-500", dark: "teal-800" };
@@ -13,10 +14,6 @@ const SearchPage = () => {
   return (
     <main className="dark:bg-gray-900">
       <Navbar />
-      <p1 className="dark:text-white text-2l text-center mb-14">
-        Displaying {artworks.length} results
-      </p1>
-      
       <div className="container mx-auto mb-20 flex py-20 gap-8">
         <aside style={{ flexBasis: "12em" }}>
           <h3 className="dark:text-gray-200 mb-3 font-semibold">Tags:</h3>
@@ -32,49 +29,45 @@ const SearchPage = () => {
           </div>
         </aside>
       
-      <div className="flex flex-wrap items-center justify-around gap-x-10 gap-y-10">
-        
-        {artworks.map(artwork => {
-          return (
-            <Link
-              to={`/artwork/${artwork.id}`}
-              key={artwork.id}
-              className={"hover:bg-gray-800"}
-            >
-              <img
-                style={{maxWidth: "20em"}}
-                className="mb-5 shadow-xl mx-auto"
-                src={artwork.image}
-                alt={artwork.name}
-              />
-              <div className="pl-3">
-                <h2 className="dark:text-white text-lg font-semibold mb-1">
-                  {artwork.name}
-                </h2>
-                <p className="dark:text-gray-300 text-sm mb-3">
-                  {artwork.description}
-                </p>
-              </div>
+      <div className="flex flex-wrap">
+        <div className="ml-20">
+          <p1 className="dark:text-white text-2l text-center mb-8 float-right">
+            Displaying {artworks.length} results
+          </p1>
+        </div>
+        <div className="flex flex-wrap items-center justify-around gap-x-10 gap-y-10">
+          {artworks.map(artwork => {
+            return (
+              <Link
+                to={`/artwork/${artwork.id}`}
+                key={artwork.id}
+                className={"hover:bg-gray-800"}
+              >
+                <img
+                  style={{maxWidth: "20em"}}
+                  className="mb-5 shadow-xl mx-auto"
+                  src={artwork.image}
+                  alt={artwork.name}
+                />
+                <div className="pl-3">
+                  <h2 className="dark:text-white text-lg font-semibold mb-1">
+                    {artwork.name}
+                  </h2>
+                  <p className="dark:text-gray-300 text-sm mb-3">
+                    {artwork.description}
+                  </p>
+                </div>
 
 
 
-            </Link>
-          );
-        })}
+              </Link>
+            );
+          })}
         </div>
         </div>
+        </div>
 
-
-
-
-
-
-
-      <footer className="py-20 bg-gray-900">
-        <h4 className="text-center dark:text-gray-300 font-semibold text-xs">
-          Page created with <Link to="/">Artsu.me</Link>
-        </h4>
-      </footer>
+        <Footer />
     </main>
   );
 };

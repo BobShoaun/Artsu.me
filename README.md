1275219c990eb565ba5fbcd26d11b2a5a3e0f9de
scheffe8 <louis.scheffer@mail.utoronto.ca>
2021-11-07 21:27:22 -0500

    updated admin artwork viewer with tags and summary

--- README.md
+++ README.md
@@ -159,7 +159,10 @@ From this page users can:
    - Description
    - Artist
    - Tags
+   - Summary
 - From this page an admin perform the following actions:
    - Ban an artwork
    - Remove a description
+   - Remove a summary
+   - Remove tags
    - Remove tags from the artwork
--- frontend/src/AdminArtworkViewer/index.jsx
+++ frontend/src/AdminArtworkViewer/index.jsx
@@ -5,9 +5,12 @@ import { Link, useParams } from "react-router-dom";
 import { users } from "../users.json";
 //replace with API call in phase 2
 
+import { X } from "react-feather";
+
 import { artworks } from "../artworks.json";
 import { useScrollToTop } from "../hooks/useScrollToTop";
 import {useAuthentication} from "../hooks/useAuthentication";
+import {tags} from "../tags.json";
 
 const AdminArtworkViewer = () => {
   const { id } = useParams();
@@ -16,6 +19,10 @@ const AdminArtworkViewer = () => {
   //replace with API call in phase 2
   const user = users.find(user => user.id === artwork.author_id);
   const [, adminUser] = useAuthentication();
+  const artworkTags = [];
+  artwork.tagids.forEach(tagid => {
+      artworkTags.push(tags.find(tag => tag.id === tagid));
+  });
   
   const otherArts = user.portfolioSettings.artworkIds.filter(ID => ID !== id).sort((a, b) => 0.5 - Math.random()) // grab artworks, remove currently displayed one, and roughly shuffle them (shuffle apporach found online)
   const otherArtsArray = []
@@ -25,7 +32,7 @@ const AdminArtworkViewer = () => {
 
   useScrollToTop();
 
-  //phase2: create method here for API call to update artwork
+  //phase2: create method here for API call to update artwork and/or tags
 
   if (adminUser.isAdmin) {
       return (
@@ -37,6 +44,9 @@ const AdminArtworkViewer = () => {
                        alt={artwork.name}
                   />
                   <h1 className="place-self-center mb-5 text-white text-center text-5xl col-span-3">{artwork.name}</h1>
+                  <p className="text-center text-gray-300 font mb-14">
+                      {artwork.summary}
+                  </p>
                   <section className="flex gap-x-10 px-5">
                       <Link to={`/admin/${user.username}`} className={"hover:bg-gray-800 flex-none w-40"}>
                           <img className="rounded-lg shadow-xl"
@@ -45,11 +55,27 @@ const AdminArtworkViewer = () => {
                           />
                           <p className="text-xl text-white text-center">{user.name}</p>
                       </Link>
-                      <p className="text-white">{artwork.description_full}</p>
-                      <p className="text-center text-gray-300 font mb-14">
-                          {artwork.summary}
-                      </p>
-
+                      <div>
+                          {artworkTags.length > 0 && (
+                              <div className="flex flex-wrap gap-3 mb-5 items-center">
+                                  {artworkTags.map(tag => (
+                                          <p
+                                              key={tag.id}
+                                              className={`flex gap-3 items-center text-gray-900 cursor-pointer font-semibold text-xs bg-${tag.color} rounded-sm px-2 py-1`}
+                                          >
+                                              #{tag.label}
+                                              <X
+                                                  size={20}
+                                                  color="white"
+                                                  strokeWidth={3}
+                                                  className="cursor-pointer"
+                                              />
+                                          </p>
+                                  ))}
+                              </div>
+                          )}
+                          <p className="text-gray-200 text-sm">{artwork.description}</p>
+                      </div>
                   </section>
                   <div className="text-right mb-10 mx-auto">
                       <button
@@ -57,7 +83,7 @@ const AdminArtworkViewer = () => {
                           Ban Artwork
                       </button>
                       <button
-                          className="text-gray-800 font-semibold bg-gray-200 hover:bg-opacity-90 bg-opacity-75 py-1 px-3 text-sm">
+                          className="text-gray-800 font-semibold bg-gray-200 hover:bg-opacity-90 bg-opacity-75 py-1 px-3 text-sm mr-2">
                           Remove Summary
                       </button>
                       <button

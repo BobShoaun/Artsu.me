8c654bbd6aa0b245894af9752ca53c5b06eee374
SR <1219361656@qq.com>
2021-12-01 14:26:16 -0500

    Update index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 import user from "./user.model.js";
 import portfolio from "./portfolio.model.js";
 import tag from "./tag.model.js";
+import artwork from "./artwork.model.js";
 
 export const User = mongoose.model("users", user);
 export const Portfolio = mongoose.model("portfolios", portfolio);
 export const Tag = mongoose.model("tags", tag);
+export const Artwork = mongoose.model("artworks", artwork);

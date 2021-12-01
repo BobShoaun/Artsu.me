3c086692ee4ae80c27c744877a61bf763d61be81
JHypatia <50211335+JHypatia@users.noreply.github.com>
2021-12-01 14:32:35 -0500

    Updated app and index to use tag routing

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import authenticationRoutes from "./routes/authentication.route.js";
 import userRoutes from "./routes/user.route.js";
 import portfolioRoutes from "./routes/portfolio.route.js";
+import tagRoutes from "./routes/tag.route.js";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(authenticationRoutes);
 app.use(userRoutes);
 app.use(portfolioRoutes);
+app.use(tagRoutes)
 
 app.get("/", (req, res) => res.send("Hello from artsu.me"));
 app.all("*", (req, res) => res.sendStatus(404));

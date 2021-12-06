cadf15ddb7d35a7fe748f5ea26346d51e9dc8885
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-12-06 17:41:35 -0500

    artwork page redirecttologin

--- frontend/src/hooks/useAuthentication.js
+++ frontend/src/hooks/useAuthentication.js
@@ -1,9 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
 import { login as authLogin, logout as authLogout } from "../store/authenticationSlice";
+import { useHistory } from "react-router";
 
 export const useAuthentication = () => {
   const dispatch = useDispatch();
   const { accessToken, user } = useSelector(state => state.authentication);
+  const history = useHistory();
 
   const login = (user, accessToken) => {
     dispatch(authLogin({ user, accessToken }));
@@ -11,6 +13,12 @@ export const useAuthentication = () => {
 
   const logout = () => {
     dispatch(authLogout());
+  };
+
+  const redirectToLogin = () => {
+    const params = new URLSearchParams();
+    params.set("destination", history.location.pathname);
+    history.push(`/login?${params}`);
   };
 
   const isLoggedIn = accessToken && user;
@@ -21,5 +29,6 @@ export const useAuthentication = () => {
     user,
     login,
     logout,
+    redirectToLogin,
   };
 };

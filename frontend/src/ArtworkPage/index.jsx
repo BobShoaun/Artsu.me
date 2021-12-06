d88187f8f70ce468bbeb30887e65d8eda1eac06f
JHypatia <50211335+JHypatia@users.noreply.github.com>
2021-12-06 15:32:48 -0500

    Fixed issue with tag keys

--- frontend/src/ArtworkPage/index.jsx
+++ frontend/src/ArtworkPage/index.jsx
@@ -148,7 +148,7 @@ const ArtworkPage = () => {
               {artworkTags.length > 0 && (
                 <div className="flex flex-wrap gap-3 mb-5">
                   {artworkTags.map(tag => (
-                    <Link key={tag.id} to={`/search/&tag=${tag.label}`}>
+                    <Link key={tag._id} to={`/search/&tag=${tag.label}`}>
                       <p
                         className={`text-gray-900 cursor-pointer font-semibold text-xs bg-${tag.color} rounded-sm px-2 py-1`}
                       >

79d1ff21b897b8f1142e9bff31d0ceb7a5fa042a
SR <1219361656@qq.com>
2021-12-05 19:32:26 -0500

    Update index.jsx

--- frontend/src/SearchPage/index.jsx
+++ frontend/src/SearchPage/index.jsx
@@ -4,73 +4,64 @@ import Footer from "../components/Footer";
 import { users } from "../users.json";
 import { tags } from "../tags.json"; */
 import { Link, useParams } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { apiUrl } from "../config";
 import axios from "axios";
 
 const SearchPage = () => {
-  const target = window.location.pathname
-    .split("search")
-    .pop()
-    .replace("/", "")
-    .replace("%20", " ")
-    .toLowerCase();
+  // link from navbar file
+  const urlParams = new URLSearchParams(window.location.pathname)
+  const art = urlParams.get('art')
+  const user = urlParams.get('usr')
+  const tag = urlParams.get('tag')
 
   const [artworks, setArtworks] = useState([]);
   const [users, setUsers] = useState([])
   const [tags, setTags] = useState([])
 
+  let length;
+
   const getSearchResult = async() => {
     try {
-      const { data: artworks } = await axios.get(`${apiUrl}/artworks`);
-      setArtworks(artworks)
-      console.log(artworks)
-      const { data: users } = await axios.get(`${apiUrl}/users`);
-      setUsers(users)
-      const { data: tags } = await axios.get(`${apiUrl}/tags`);
+      // set up tags to display
+      const { data: tags } = await axios.get(`${apiUrl}/tags`)
       setTags(tags)
+
+      if (art) {
+        const { data: artworks } = await axios.get(`${apiUrl}/artworks?name=${art}`);
+        setArtworks(artworks)
+        length = artworks.length
+      }
+      else if (user) {
+        const { data: users } = await axios.get(`${apiUrl}/users?name=${user}`);
+        setUsers(users)
+        length = users.length
+      }
+      else if (tag) {
+        const { data: artworks } = await axios.get(`${apiUrl}/artworks?tags=${[tag]}`);
+        setArtworks(artworks)
+        length = artworks.length
+      }
+      else {
+        const { data: artworks } = await axios.get(`${apiUrl}/artworks`);
+        setArtworks(artworks)
+        length = artworks.length
+      }
     } catch(e) {
       console.log(e);
     }
   }
 
-  /* eslint-disable no-unused-expressions */
-  getSearchResult();
+  getSearchResult()
+
+  let msg = "results";
+  if (length <= 1) {
+    msg = "result";
+  }
 
   function displaySearchResult() {
-    let length = artworks.length;
-    /* if (target.substr(0, 5) === "&tag=") {
-      let tagFiltered = tags.filter(
-        // API calls to get the tag searched
-        tag => tag.label.toLowerCase() === target.substr(5)
-      );
-      let tag = tagFiltered.map(tag => tag.id)[0];
-      artworksFiltered = artworks.filter(
-        (
-          artwork // API calls to get artworks with specified tag
-        ) => artwork.tagIds.includes(tag)
-      );
-      length = artworksFiltered.length;
-    } else  */
-    if (target.substr(0, 5) === "&art=") {
-      /* artworksFiltered = artworks.filter(
-        (
-          artwork // API calls to get arts searched
-        ) => artwork.name.toLowerCase().includes(target.substr(5))
-      ); */
-      length = artworks.length;
-    } else if (target.substr(0, 5) === "&usr=") {
-      /* usersFiltered = users.filter(
-        (
-          user // API calls to get users searched
-        ) => user.name.toLowerCase().includes(target.substr(5))
-      ); */
-      length = users.length;
-      let msg = "results";
-      if (users.length <= 1) {
-        msg = "result";
-      }
+    if (user) {
       return (
         <div>
           <div className="ml-20 w-full mb-10">
@@ -110,47 +101,39 @@ const SearchPage = () => {
         </div>
       );
     }
-    let msg = "results";
-    if (artworks.length <= 1) {
-      msg = "result";
+    else {
+      return (
+        <div>
+          <div className="ml-20 w-full mb-10">
+            <p className="dark:text-white text-2l w-100% float-left mr-10">
+              Displaying {length} {msg}
+            </p>
+          </div>
+          <div className="flex flex-wrap justify-around gap-x-10 gap-y-10">
+            {artworks.map(artwork => {
+              return (
+                <Link
+                  to={`/artwork/${artwork.id}`}
+                  key={artwork.id}
+                  className={"hover:bg-gray-800"}>
+                  <img className="mb-5 max-w-xs shadow-xl mx-10 mt-5 max-w-20"
+                    src={artwork.imageUrl}
+                    alt={artwork.name}/>
+                  <div className="pl-3">
+                    <h2 className="dark:text-white text-lg font-semibold mb-1 ml-10">
+                      {artwork.name}
+                    </h2>
+                    <p className="dark:text-gray-300 text-sm m-5 ml-10">
+                      {artwork.summary}
+                    </p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      );
     }
-    return (
-      <div>
-        <div className="ml-20 w-full mb-10">
-          <p className="dark:text-white text-2l w-100% float-left mr-10">
-            Displaying {length} {msg}
-          </p>
-        </div>
-        <div className="flex flex-wrap justify-around gap-x-10 gap-y-10">
-          {artworks.map(artwork => {
-            return (
-              <Link
-                to={`/artwork/${artwork.id}`}
-                key={artwork.id}
-                className={"hover:bg-gray-800"}
-              >
-                <img
-                  className="mb-5 max-w-xs shadow-xl mx-10 mt-5 max-w-20"
-                  src={artwork.imageUrl}
-                  alt={artwork.name}
-                />
-                <div className="pl-3">
-                  <h2
-                    className="dark:text-white text-lg font-semibold mb-1
-                                ml-10"
-                  >
-                    {artwork.name}
-                  </h2>
-                  <p className="dark:text-gray-300 text-sm m-5 ml-10">
-                    {artwork.summary}
-                  </p>
-                </div>
-              </Link>
-            );
-          })}
-        </div>
-      </div>
-    );
   }
 
   return (
@@ -160,19 +143,17 @@ const SearchPage = () => {
         <aside className="search-tag-aside">
           <h3 className="dark:text-gray-200 mb-3 font-semibold">Tags:</h3>
           <div className=" flex-shrink-0 flex-wrap gap-3">
-            {/* {tags.map(tag => {
+            { tags.map(tag => {
               return (
-                <Link to={`/search/&tag=${tag.label}`}>
-                  <p
-                    key={tag.id}
-                    className={`text-gray-700 cursor-pointer font-semibold 
-                              text-sm bg-${tag.color} rounded-sm px-2 py-2 my-5`}
-                  >
+                <Link to={`/search/&tag=${tag.id}`}
+                      key={tag.id}>
+                  <p className={`text-gray-700 cursor-pointer font-semibold 
+                              text-sm bg-${tag.color} rounded-sm px-2 py-2 my-5`}>
                     #{tag.label}
                   </p>
                 </Link>
               );
-            })} */}
+            }) }
           </div>
         </aside>
         {displaySearchResult()}

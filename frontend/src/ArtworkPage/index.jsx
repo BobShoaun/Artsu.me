9f1a06944dd4ba60d072f29df1b66f108a9eb179
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-12-06 21:18:04 -0500

    configure tailwind purge

--- frontend/src/ArtworkPage/index.jsx
+++ frontend/src/ArtworkPage/index.jsx
@@ -151,7 +151,7 @@ const ArtworkPage = () => {
               {artworkTags.length > 0 && (
                 <div className="flex flex-wrap gap-3 mb-5">
                   {artworkTags.map(tag => (
-                    <Link key={tag._id} to={`/search/&tag=${tag.label}`}>
+                    <Link key={tag._id} to={`/search?tag=${tag._id}`}>
                       <p
                         className={`text-gray-900 cursor-pointer font-semibold text-xs bg-${tag.color} rounded-sm px-2 py-1`}
                       >

bc6dfdbe0690b89b0fd283ca2b741eb4343899b2
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-12-07 21:56:17 -0500

    remove comments

--- backend/src/models/portfolio.model.js
+++ backend/src/models/portfolio.model.js
@@ -16,32 +16,6 @@ const artworkArtistValidator = {
   message: ({ value }) => `${value} userId does not belong to portfolio's userId`,
 };
 
-// TODO: check if duplicate artworkId
-
-// const experience = new Schema(
-//   {
-//     company: { type: String },
-//     position: { type: String },
-//     startDate: { type: Date },
-//     endDate: { type: Date },
-//     description: { type: String },
-//     artworkIds: [
-//       {
-//         type: Schema.ObjectId,
-//         ref: "artworks",
-//         validate: artworkExistValidator,
-//       },
-//     ],
-//   },
-//   { versionKey: false, toJSON: { virtuals: true }, toObject: { virtuals: true } }
-// );
-
-// experience.virtual("artworks", {
-//   ref: "artworks",
-//   localField: "artworkIds",
-//   foreignField: "_id",
-// });
-
 const portfolio = new Schema(
   {
     userId: { type: Schema.ObjectId, required: true, ref: "users", unique: true, immutable: true },
@@ -82,7 +56,7 @@ const portfolio = new Schema(
         isVisible: { type: Boolean, required: true, default: false },
       },
       project: {
-        layoutId: {Number, default: 0 },
+        layoutId: { Number, default: 0 },
         artworkIds: [
           {
             type: Schema.ObjectId,
@@ -112,11 +86,4 @@ portfolio.virtual("section.project.artworks", {
   localField: "section.project.artworkIds",
   foreignField: "_id",
 });
-
-// portfolio.virtual("section.experience.experiences", {
-//   ref: "experiences",
-//   localField: "section.experience.experiences",
-//   foreignField: "_id",
-// });
-
 export default portfolio;

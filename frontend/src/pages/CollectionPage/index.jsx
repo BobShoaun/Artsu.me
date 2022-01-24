2d01fd0f683eb5ead2fa8300f2aa852c7aa0fc01
cemarciano <cemarciano@poli.ufrj.br>
2022-01-24 12:17:57 -0300

    Format items in collection

--- frontend/src/pages/CollectionPage/index.jsx
+++ frontend/src/pages/CollectionPage/index.jsx
@@ -95,8 +95,11 @@ const CollectionPage = () => {
                   <NFThumb artwork={artwork}/>
               </div>
               <div className="text-center">
-                <h2 className="dark:text-white text-lg font-semibold mb-1">{artwork.name}</h2>
-                <p className="dark:text-gray-300 text-sm mb-4">{artwork.floor}</p>
+                <h2 className="float-left ml-4 dark:text-white text-lg font-semibold mb-1">{artwork.name}</h2>
+                <p className="float-right mr-4 flex dark:text-gray-300 mb-4">
+                  <img src="/icons/ethereum logo.svg" className="h-5 dark:brightness-200" alt="eth logo" />
+                  <span className="ml-2 text-md">{artwork.floor}</span>
+                </p>
               </div>
             </div>
           ))}
